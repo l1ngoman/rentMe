3470322e4c487eab45e11bcd9bee94a555b78418
l1ngoman <41375558+l1ngoman@users.noreply.github.com>
2018-10-05 12:01:04 -0700

    started creating dataset upload function

--- initialCode.js
+++ initialCode.js
@@ -1,1 +1,12 @@
+//load initial dataset
+//part number, name, description, status (on_rent/on_hand)
+function makeItems(partNum,desc,status){
+  var newItem = {
+    partNum: partNum,
+    desc: desc,
+    status: status,
+    getInfo: function(){console.log(`Part No: ${partNum}\nDesc: ${desc}\nStatus: ${status}`)}
+  }
+}
 
+var item1 = makeItems
